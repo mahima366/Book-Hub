4de83d8f910c1cf5b48fae744c0e2262702952fd
mahima366 <156400922+mahima366@users.noreply.github.com>
2025-02-10 13:17:03 +0530

    Create bookhub.sql

---
+++ bookhub.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS LibraryDB;
+
+USE LibraryDB;
+
+CREATE TABLE Books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    isbn VARCHAR(20) NOT NULL UNIQUE,
+    year INT NOT NULL,
+    genre VARCHAR(100),
+    available BOOLEAN NOT NULL
+);
